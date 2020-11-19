756c6a0e387c6dad891bed6600dc9619d961de46
Kavin P <kavin_p@bangchak.co.th>
2020-11-19 10:54:34 +0700

    Update bot_settings.php

--- config/bot_settings.php
+++ config/bot_settings.php
@@ -2,6 +2,5 @@
 /// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
 define('LINE_MESSAGE_CHANNEL_ID','1655255429');
 define('LINE_MESSAGE_CHANNEL_SECRET','9646c8941f8459f4accdfa99a1bd61e9');
-define('LINE_MESSAGE_ACCESS_TOKEN','u2KWrzubwWKd1yqE2Cc4gsmjGaqIxIBcF/IGTtihlPE4l524lAT3+xmCz+Y743ypzB7zKqhmVRyxhcXNoB1YOCNAuR8qbECDivkQBvDXMo6M57WydaSuTrWdpbz221vqpfvP1rPrBvGnMBGZ16cYiQdB04t89/1O/w1cDnyilFU=
-');
+define('LINE_MESSAGE_ACCESS_TOKEN','u2KWrzubwWKd1yqE2Cc4gsmjGaqIxIBcF/IGTtihlPE4l524lAT3+xmCz+Y743ypzB7zKqhmVRyxhcXNoB1YOCNAuR8qbECDivkQBvDXMo6M57WydaSuTrWdpbz221vqpfvP1rPrBvGnMBGZ16cYiQdB04t89/1O/w1cDnyilFU=');
 ?>
